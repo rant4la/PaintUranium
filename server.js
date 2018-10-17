3ce1970dbe99dd21b13bdc31a8676163a3ef5019
UisDangerouz <33716618+UisDangerouz@users.noreply.github.com>
2018-10-17 21:10:20 +0300

    CORS should work now

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var serverPort = 8080;
 
 //APP SETUP
 var app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var server = app.listen(8080, function() {
 	console.log("Server started on port: " + serverPort)
 });
