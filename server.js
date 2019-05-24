7a38dc16aea4bc46d6affd80353b65efb4ee029d
UisDangerouz <33716618+UisDangerouz@users.noreply.github.com>
2019-05-24 23:47:18 +0300

    heroku fix final maybe

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
+    res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+	
 });
 app.use(express.static('public'));
 
