045c32b40a1dec243f939fc86ae327e001def33d
UisDangerouz <33716618+UisDangerouz@users.noreply.github.com>
2019-05-24 23:24:59 +0300

    another another fix to hopefully work on Heroku

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ var serverPort = process.env.PORT || 80;
 //APP SETUP
 var app = express();
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
